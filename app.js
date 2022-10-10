c4945f371e4748d16b672dcd075dea8b65d61964
JuanmaZimm <juanmanuelzimmermann@gmail.com>
2022-10-10 16:21:52 -0300

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
 
-app.listen(3000, () => console.log('Server on'));
+//Levantar nuestro servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
